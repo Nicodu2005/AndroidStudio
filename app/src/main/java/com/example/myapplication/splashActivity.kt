0d87b244206b089c4b5b498baf62d625e8a0955b
Nicodu2005 <nicolaspoveda000@gmail.com>
2025-02-23 11:14:36 -0500

    Carga inicial del proyecto

---
+++ app/src/main/java/com/example/myapplication/splashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity (){
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+}
